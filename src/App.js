1e82e400b112bab3b354a854c5bc0bea7f76a23c
shubhdevnsg01 <88392448+shubhdevnsg01@users.noreply.github.com>
2023-12-30 13:09:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,88 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+// LoginPage.js
 
-const ProductList = () => {
-  const [products, setProducts] = useState([]);
+import React, { useState } from 'react';
+import './App.css';
 
-  useEffect(() => {
- 
-    const apiURL = "https://s3.amazonaws.com/open-to-cors/assignment.json";
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-    axios.get(apiURL)
-      .then(response => {
-       
-        if (response.status !== 200) {
-          throw new Error(`Failed to fetch data. Status code: ${response.status}`);
-        }
-        return response.data;
-      })
-      .then(data => {
-      
-        const productsArray = Object.values(data.products);
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
 
-     
-        const sortedProducts = productsArray.sort((a, b) => b.popularity - a.popularity);
+    // Email validation using a simple regex pattern
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(value)) {
+      setErrorMessage('Enter a valid email address');
+    } else {
+      setErrorMessage('');
+    }
 
-      
-        setProducts(sortedProducts);
-      })
-      .catch(error => {
-        console.error(error.message);
-      });
-  }, []); 
+    setEmail(value);
+  };
+
+  const handlePasswordChange = (e) => {
+    const value = e.target.value;
+
+    // Password validation
+    if (/[^a-zA-Z0-9@]/.test(value)) {
+      setErrorMessage('Password should not contain special characters other than @');
+    } else if (!/[A-Z]/.test(value) || !/[0-9]/.test(value)) {
+      setErrorMessage('Password must contain at least one uppercase letter and one number');
+    } else {
+      setErrorMessage('');
+    }
+
+    setPassword(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Additional logic for handling login
+    // You can perform authentication here
+
+    // For this example, let's just log the credentials
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
 
   return (
-    <div>
-      <h1>Product List</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Subcategory</th>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Popularity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map(product => (
-            <tr key={product.title}>
-              <td>{product.subcategory}</td>
-              <td>{product.title}</td>
-             
-              <td>{product.price}</td>
-              <td>{product.popularity}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="login-container">
+      {/* Logo Container */}
+      <div className="logo-container">
+        <div className="logo"></div>
+      </div>
+
+      {/* Login Form */}
+      <h2 className="login-heading"></h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="text"
+            value={email}
+            placeholder='Username'
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            placeholder='Password'
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
 
-export default ProductList;
+export default LoginPage;
